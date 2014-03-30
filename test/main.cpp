d56f96d40287a5fc4cb6abbbd9305b57c496ad0a
user <user@Kenji-no-MacBook-Pro.local>
2014-03-30 23:13:43 +0900

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by user on 2014/03/30.
+//  Copyright (c) 2014年 test1. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
